7efa292c70e4dad11c2168f929336dba9e0d6828
Diogo-d-Souza <di_smgomes@hotmail.com>
2024-09-22 16:02:10 -0300

    feat: Repositories created

---
+++ src/main/java/com/diogosmg/file_manager/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.diogosmg.file_manager.repositories;
+
+import com.diogosmg.file_manager.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    UserDetails findByEmail(String email);
+}
